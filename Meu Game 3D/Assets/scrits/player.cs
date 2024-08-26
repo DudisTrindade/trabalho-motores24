e790959b76e0a6487a2504fac2c4c3cb253f2d5a
DudisTrindade <eduarda.trindade@escolar.ifrn.edu.br>
2024-08-26 16:18:35 -0300

    duda

--- Meu Game 3D/Assets/scrits/player.cs
+++ Meu Game 3D/Assets/scrits/player.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using Vector3 = UnityEngine.Vector3;
 
 public class player : MonoBehaviour
-{  
+{
     public int velocidade = 10;
+
     Rigidbody rb;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,6 +23,9 @@ public class player : MonoBehaviour
         float v = Input.GetAxis("Vertical");
         Vector3 direcao = new Vector3(h, 0, v);
         rb.AddForce(direcao * velocidade * Time.deltaTime, ForceMode.Impulse);
-
+        if (transform.position.y < -5)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
     }
 }
